ff22084f95aa2aaf1147faa5d13489df5448f06a
Javi <javiermespino@gmail.com>
2023-03-16 12:31:51 +0000

    Test(red): check_that_number_3_returns_fizz

---
+++ src/test/fizzbuzz.test.ts
@@ -0,0 +1,8 @@
+import { fizzbuzz } from "../core/fizzbuzz";
+
+describe('Fizzbuzz Test', () => {
+    it("check that number 3 returns fizz", () => {
+        const fizzbuzzResult = fizzbuzz();
+        expect(fizzbuzzResult[2]).toBe("fizz")
+    })
+});
