18a96db41ae8ccf1e3f749241db4083e6cf56cc9
Javi <javiermespino@gmail.com>
2023-03-16 13:04:22 +0000

    Test(red): check_that_number_5_returns_buzz

--- src/core/fizzbuzz.ts
+++ src/core/fizzbuzz.ts
@@ -1,9 +1,10 @@
 export const fizzbuzz = () => {
-    const array = [1, 2, 3, 4, 5];
-    return array.map((num) => {
+    const array = [1, 2, 3];
+    const fizzbuzzResult: (string|number)[] = array.map((num) => {
         if (num % 3 === 0) {
             return "fizz";
         }
         return num;
     });
+    return fizzbuzzResult;
 };
--- src/test/fizzbuzz.test.ts
+++ src/test/fizzbuzz.test.ts
@@ -3,6 +3,13 @@ import { fizzbuzz } from "../core/fizzbuzz";
 describe('Fizzbuzz Test', () => {
     it("check that number 3 returns fizz", () => {
         const fizzbuzzResult = fizzbuzz();
-        expect(fizzbuzzResult.find((num, pos) => num === "fizz" && (pos + 1) % 3 === 0)) 
+        const fizz = fizzbuzzResult.find((num, pos) => num === "fizz" && (pos + 1) % 3 === 0) 
+        expect(fizz).toBe("fizz");
+    })
+
+    it("check that number 5 returns buzz", () => {
+        const fizzbuzzResult = fizzbuzz();
+        const buzz = fizzbuzzResult.find((num, pos) => num === "buzz" && (pos + 1) % 5 === 0)
+        expect(buzz).toBe("buzz");
     })
 });
