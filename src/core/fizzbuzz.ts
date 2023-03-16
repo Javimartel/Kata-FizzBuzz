7ba4f6961f5f152e971917f3f984c641b75e56dd
Javi <javiermespino@gmail.com>
2023-03-16 13:33:17 +0000

    Test(green): check_that_number_15_returns_fizzbuzz

--- src/core/fizzbuzz.ts
+++ src/core/fizzbuzz.ts
@@ -5,7 +5,9 @@ export const fizzbuzz = () => {
 };
 
 const whoFizzBuzzElementIs = (num: number) => {
-    if (num % 3 === 0) {
+    if (num % 3 === 0 && num % 5 === 0) {
+        return "fizzbuzz";
+    } else if (num % 3 === 0) {
         return "fizz";
     } else if (num % 5 === 0) {
         return "buzz";
