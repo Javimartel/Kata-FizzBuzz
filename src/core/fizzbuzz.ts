b3163390300a1f87fada56a5958704cfb9e337b9
Javi <javiermespino@gmail.com>
2023-03-16 13:16:14 +0000

    Test(refactor): check_that_number_5_returns_buzz

--- src/core/fizzbuzz.ts
+++ src/core/fizzbuzz.ts
@@ -1,12 +1,13 @@
 export const fizzbuzz = () => {
     const array = [1, 2, 3, 4, 5];
-    const fizzbuzzResult: (string|number)[] = array.map((num) => {
-        if (num % 3 === 0) {
-            return "fizz";
-        } else if (num % 5 === 0) {
-            return "buzz";
-        }
-        return num;
-    });
-    return fizzbuzzResult;
+    return array.map((num) => whoFizzBuzzElementIs(num));
+};
+
+const whoFizzBuzzElementIs = (num: number) => {
+    if (num % 3 === 0) {
+        return "fizz";
+    } else if (num % 5 === 0) {
+        return "buzz";
+    }
+    return num;
 };
