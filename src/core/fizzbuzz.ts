2d0bbf34d810a736848b8be3cdf80db3dcf618f9
Javi <javiermespino@gmail.com>
2023-03-16 14:53:23 +0000

    Test(refactor): check_that_a_number_is_fizzfizz_if_it_contains_3_and_3_and_is_divisible_by_3

--- src/core/fizzbuzz.ts
+++ src/core/fizzbuzz.ts
@@ -6,18 +6,23 @@ export const fizzbuzz = () => {
 
 export const whoFizzBuzzElementIs = (num: number) => {
     const numberStringify = num.toString();
+    const isDivisibleBy3 = num % 3 === 0;
+    const isDivisibleBy5 = num % 5 === 0;
+
     let fizzbuzz = "";
-    if (num % 3 === 0 ) {
+
+    if (isDivisibleBy3) {
         fizzbuzz += "fizz";
     }
     if (numberStringify.includes("3")) {
         fizzbuzz += "fizz";
     }
-    if (num % 5 === 0) {
+    if (isDivisibleBy5) {
         fizzbuzz += "buzz";
     }
     if (numberStringify.includes("5")) {
         fizzbuzz += "buzz";
     }
+
     return fizzbuzz === "" ? num : fizzbuzz;
 };
