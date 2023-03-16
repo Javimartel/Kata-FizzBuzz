6c329fae6f51f97d45796a6c3f852a261ba215a7
Javi <javiermespino@gmail.com>
2023-03-16 13:05:54 +0000

    Test(green): check_that_number_5_returns_buzz

--- src/core/fizzbuzz.ts
+++ src/core/fizzbuzz.ts
@@ -1,8 +1,10 @@
 export const fizzbuzz = () => {
-    const array = [1, 2, 3];
+    const array = [1, 2, 3, 4, 5];
     const fizzbuzzResult: (string|number)[] = array.map((num) => {
         if (num % 3 === 0) {
             return "fizz";
+        } else if (num % 5 === 0) {
+            return "buzz";
         }
         return num;
     });
