028aab7a8e383e9902e6719402f5be46414ccaa6
Javi <javiermespino@gmail.com>
2023-03-16 12:56:07 +0000

    Test(refactor_test): check_that_number_3_returns_fizz

--- src/core/fizzbuzz.ts
+++ src/core/fizzbuzz.ts
@@ -1,5 +1,5 @@
 export const fizzbuzz = () => {
-    const array = [1, 2, 3];
+    const array = [1, 2, 3, 4, 5];
     return array.map((num) => {
         if (num % 3 === 0) {
             return "fizz";
--- src/test/fizzbuzz.test.ts
+++ src/test/fizzbuzz.test.ts
@@ -3,6 +3,6 @@ import { fizzbuzz } from "../core/fizzbuzz";
 describe('Fizzbuzz Test', () => {
     it("check that number 3 returns fizz", () => {
         const fizzbuzzResult = fizzbuzz();
-        expect(fizzbuzzResult[2]).toBe("fizz")
+        expect(fizzbuzzResult.find((num, pos) => num === "fizz" && (pos + 1) % 3 === 0)) 
     })
 });
